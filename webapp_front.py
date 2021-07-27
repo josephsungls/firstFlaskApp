f23be5c562cf52941cdd282223b9db228914717f
josephsungls <josephsung@smu.edu.sg>
2021-07-27 23:55:34 +0800

    Update webapp_front.py

--- webapp_front.py
+++ webapp_front.py
@@ -5,9 +5,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    now=datetime.now()
-    current_time=now.strftime("%H:%M:%S")
-    return "Hi there, the time is " + current_time
+    #now=datetime.now()
+    #current_time=now.strftime("%H:%M:%S")
+    #return "Hi there, the time is " + current_time
+    return "Hello World"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="172.31.31.208", port=80)
